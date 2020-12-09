f5737357258ee7c2f36bed42af0bc0d54be69a9e
MarcoBatman <54103563+MarcoBatman@users.noreply.github.com>
2020-12-09 13:43:02 +0100

    gewrgrewgerw

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -3,9 +3,8 @@ import processing.core.PApplet;
 import java.util.ArrayList;
 
 public class Graph {
-
     ArrayList<Integer> highesttList = new ArrayList<Integer>();
-
+  int j;
     PApplet p;
     Graph(PApplet p){
         this.p=p;
@@ -13,23 +12,24 @@ public class Graph {
     public void display(){
         for (int i = 0; i < highesttList.size() ; i++) {
             p.fill(0);
-            if (highesttList.get(i)==1130){
+            if (highesttList.get(highesttList.size()-1-i)==1130){
                 p.fill(255,0,0);
             }
-            p.textSize(5);
-            p.textAlign(p.CENTER);
+            p.textSize(15);
             p.pushMatrix();
             p.rotate((float)1.57079633*3);
-            p.text(highesttList.get(i), -300+highesttList.get(i)/3,15+15*i);
+            int generaton = j-i;
+            p.text("Generation " + generaton + ": " + highesttList.get(highesttList.size()-1-i), -1130/5-175+highesttList.get(highesttList.size()-1-i)/5,15+15*i);
             p.popMatrix();
-           // p.rect(10+i*15,p.height,10,-highesttList.get(i)/3);
+            p.rect(5+i*15,p.height,10,-highesttList.get(highesttList.size()-1-i)/5);
+            if(5+i*15>p.width)
+                highesttList.remove(0);
+
         }
-
-
     }
     public void getHighest(int highest){
     highesttList.add(highest);
-
+    j++;
     }
 
 }
